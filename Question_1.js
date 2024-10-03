7d98bb3daac05dff49d0058a2dd21f6358ba6a94
dhivyaG082 <dhivyadhi082@gmail.com>
2024-10-03 23:21:40 +0530

    add node.js_assignment1 master

---
+++ Question_1.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const data = fs.readFileSync('file.txt', 'utf8');
+console.log(data); 
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
+console.log('This will print first, non-blocking execution');
